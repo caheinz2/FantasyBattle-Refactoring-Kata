306d4fe8e36135a66376275b4c396b81883d7edd
Caleb Heinz <caleb.heinz@7factor.io>
2026-01-05 12:21:33 -0500

    Wrote tests to cover base damage logic.

--- typescript/tests/Player.spec.ts
+++ typescript/tests/Player.spec.ts
@@ -1,19 +1,109 @@
+import { BasicBuff } from '../src/BasicBuff';
+import { BasicItem } from '../src/BasicItem';
+import { Equipment } from '../src/Equipment';
 import { Inventory } from '../src/Inventory';
-import { Stats } from '../src/Stats';
-import { SimpleEnemy } from '../src/SimpleEnemy';
 import { Player } from '../src/Player';
-import { Damage } from '../src/Damage';
+import { SimpleArmor } from '../src/SimpleArmor';
+import { SimpleEnemy } from '../src/SimpleEnemy';
+import { Stats } from '../src/Stats';
 
 describe('Player', () => {
 
-    // TODO: test is not finished!
 
-    it('calculates damage', () => {
-        const inventory = new Inventory(null!!);
-        const stats = new Stats(0);
-        const enemy = new SimpleEnemy(null!!, null!!);
+    describe('calculateDamage', () => {
+        describe('base damage', () => {
+            const stats = new Stats(10);
 
-        const damage: Damage = new Player(inventory, stats).calculateDamage(enemy);
-        expect(damage.amount).toBe(10);
-    });
-})
+            it('includes base damage from left hand', () => {
+                const equipment = new Equipment(
+                  new BasicItem('leftHandItem', 1, 0),
+                  new BasicItem('rightHandItem', 0, 0),
+                  new BasicItem('headItem', 0, 0),
+                  new BasicItem('feetItem', 0, 0),
+                  new BasicItem('chestItem', 0, 0),
+                );
+                const player = buildPlayer(equipment, stats);
+                const enemy = buildEnemy();
+
+                const damage = player.calculateDamage(enemy);
+
+                expect(damage.amount).toBe(1);
+            });
+
+            it('includes base damage from right hand', () => {
+                const equipment = new Equipment(
+                  new BasicItem('leftHandItem', 0, 0),
+                  new BasicItem('rightHandItem', 1, 0),
+                  new BasicItem('headItem', 0, 0),
+                  new BasicItem('feetItem', 0, 0),
+                  new BasicItem('chestItem', 0, 0),
+                );
+                const player = buildPlayer(equipment, stats);
+                const enemy = buildEnemy();
+
+                const damage = player.calculateDamage(enemy);
+
+                expect(damage.amount).toBe(1);
+            });
+
+            it('includes base damage from head', () => {
+                const equipment = new Equipment(
+                  new BasicItem('leftHandItem', 0, 0),
+                  new BasicItem('rightHandItem', 0, 0),
+                  new BasicItem('headItem', 1, 0),
+                  new BasicItem('feetItem', 0, 0),
+                  new BasicItem('chestItem', 0, 0),
+                );
+                const player = buildPlayer(equipment, stats);
+                const enemy = buildEnemy();
+
+                const damage = player.calculateDamage(enemy);
+
+                expect(damage.amount).toBe(1);
+            });
+
+            it('includes base damage from feet', () => {
+                const equipment = new Equipment(
+                  new BasicItem('leftHandItem', 0, 0),
+                  new BasicItem('rightHandItem', 0, 0),
+                  new BasicItem('headItem', 0, 0),
+                  new BasicItem('feetItem', 1, 0),
+                  new BasicItem('chestItem', 0, 0),
+                );
+                const player = buildPlayer(equipment, stats);
+                const enemy = buildEnemy();
+
+                const damage = player.calculateDamage(enemy);
+
+                expect(damage.amount).toBe(1);
+            });
+
+            it('includes base damage from chest', () => {
+                const equipment = new Equipment(
+                  new BasicItem('leftHandItem', 0, 0),
+                  new BasicItem('rightHandItem', 0, 0),
+                  new BasicItem('headItem', 0, 0),
+                  new BasicItem('feetItem', 0, 0),
+                  new BasicItem('chestItem', 1, 0),
+                );
+                const player = buildPlayer(equipment, stats);
+                const enemy = buildEnemy();
+
+                const damage = player.calculateDamage(enemy);
+
+                expect(damage.amount).toBe(1);
+            });
+        });
+    })
+
+    function buildPlayer(equipment: Equipment, stats: Stats) {
+        return new Player(new Inventory(equipment), stats);
+    }
+
+    function buildEnemy() {
+        const armor = new SimpleArmor(0);
+        const buff = new BasicBuff(0, 0);
+
+        return new SimpleEnemy(armor, [buff]);
+    }
+});
